52d4517d75f20227d398e2f0891535f23f615207
Alexander Gorny <test.automation.developer@gmail.com>
2017-01-31 20:48:24 -0600

    Optimize imports in TesBase

--- addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package ru.stqua.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqua.pft.addressbook.appmanager.ApplicationManager;
@@ -9,7 +10,7 @@ import ru.stqua.pft.addressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(org.openqa.selenium.remote.BrowserType.CHROME);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
 
   @BeforeMethod
