e391a7ac802eac4e1d4ab27f056476af3a2a09ea
Alexander Gorny <test.automation.developer@gmail.com>
2017-01-31 20:46:32 -0600

    Add Homework 7 for Lesson 3

--- addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/TestBase.java
@@ -1,17 +1,15 @@
 package ru.stqua.pft.addressbook.tests;
 
-import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqua.pft.addressbook.appmanager.ApplicationManager;
-import ru.stqua.pft.addressbook.model.ContactData;
 
 /**
  * Created by Alexander Gorny on 1/24/2017.
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(org.openqa.selenium.remote.BrowserType.CHROME);
 
 
   @BeforeMethod
