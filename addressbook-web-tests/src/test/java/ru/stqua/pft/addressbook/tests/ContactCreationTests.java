131192202a01f7198523792d0721544577aec53c
Alexander Gorny <test.automation.developer@gmail.com>
2017-02-10 20:19:08 -0600

    Add Contact Creation Test with lambda. Video 9

--- addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/ContactCreationTests.java
@@ -12,20 +12,14 @@ public class ContactCreationTests extends TestBase{
   @Test
   public void testContactCreation() {
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("Alex", "Gorny", "Cool Woker", "Mr", "GE", "New Orleans", "444-555-6666", "cool@mail.com", "woker@mail.com", "www.homepage.com", "test1");
+    ContactData contact = new ContactData("Alexander", "Gorny", "Cool Woker", "Mr", "GE", "New Orleans", "444-555-6666", "cool@mail.com", "woker@mail.com", "www.homepage.com", "test1");
     app.getContactHelper().createContact(contact, true);
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
 
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData c : after){
-      if (c.getId() > max){
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
