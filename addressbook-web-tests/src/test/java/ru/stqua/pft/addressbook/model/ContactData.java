77cc9a87072374bcd3b73921060639593038fcc9
Alexander Gorny <test.automation.developer@gmail.com>
2017-03-04 19:51:08 -0600

    Add Lesson 7. Video 3

--- addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/model/ContactData.java
@@ -3,47 +3,95 @@ package ru.stqua.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name="addressbook")
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name="id")
   private int id = Integer.MAX_VALUE;;
+
   @Expose
+  @Column(name="firstname")
   private String firsName;
+
   @Expose
+  @Column(name="lastname")
   private String lastName;
+
   @Expose
+  @Column(name="nickname")
   private String nickname;
+
   @Expose
+  @Column(name="title")
   private String title;
+
   @Expose
+  @Column(name="company")
   private String company;
+
   @Expose
+  @Column(name="address")
+  @Type(type="text")
   private String address;
+
   @Expose
+  @Column(name="home")
+  @Type(type="text")
   private String homePhone;
+
   @Expose
+  @Column(name="mobile")
+  @Type(type="text")
   private String mobilePhone;
+
   @Expose
+  @Column(name="work")
+  @Type(type="text")
   private String workPhone;
 
+  @Transient
   private String allPhones;
+
   @Expose
+  @Column(name="email")
+  @Type(type="text")
   private String email;
+
   @Expose
+  @Column(name="email2")
+  @Type(type="text")
   private String email2;
+
   @Expose
+  @Column(name="email3")
+  @Type(type="text")
   private String email3;
 
+  @Transient
   private String allEmails;
+
   @Expose
+  @Column(name="homepage")
+  @Type(type="text")
   private String urlHomePage;
 
+  @Transient
   private String group;
+
+  @Transient
   private String allContactData;
-  private File photo;
+
+  @Column(name="photo")
+  @Type(type="text")
+  private String photo;
 
   public int getId() {
     return id;
@@ -118,7 +166,7 @@ public class ContactData {
   }
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withFirsName(String firsName) {
@@ -212,7 +260,7 @@ public class ContactData {
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
