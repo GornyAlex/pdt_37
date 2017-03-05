b41fc82c53443d75c3a80a5aa8020227cdfac5bd
Alexander Gorny <test.automation.developer@gmail.com>
2017-03-05 15:26:43 -0600

    Add Lesson 7. Video 5

--- addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqua/pft/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package ru.stqua.pft.addressbook.tests;
 
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +9,14 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqua.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqua.pft.addressbook.model.GroupData;
+import ru.stqua.pft.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.Matchers.equalTo;
 
 /**
  * Created by Alexander Gorny on 1/24/2017.
@@ -43,6 +49,17 @@ public class TestBase {
   @AfterMethod(alwaysRun = true)
   public void logTestStop(Method m){
     logger.info("Stop test " + m.getName());
+  }
+
+
+  public void verifyGroupListInUI() {
+    if (Boolean.getBoolean("verifyUI"))  {
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+      MatcherAssert.assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
   }
 
 }
